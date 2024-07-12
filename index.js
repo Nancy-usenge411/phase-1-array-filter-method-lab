bf124000255d9372d68b8be851d1b4aa357aad95
Nancy Usenge <nancyusenge411@gmail.com>
2024-07-13 01:47:25 +0300

    commit array-filter-lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+function findMatching(drivers, name) {
+    const lowerCaseName = name.toLowerCase();
+    
+    const matchingDrivers = drivers.filter(driver => {
+        // Convert each driver's name to lowercase for case insensitivity
+        const lowerCaseDriverName = driver.toLowerCase();
+        
+        return lowerCaseDriverName.includes(lowerCaseName);
+    });
+    
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, query) {
+    const lowerCaseQuery = query.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => {
+        const lowerCaseDriver = driver.toLowerCase();
+        return lowerCaseDriver.startsWith(lowerCaseQuery);
+    });
+    return matchingDrivers;
+}
+
+function matchName(drivers, name) {
+    const matchingDrivers = drivers.filter(driver => {
+        return driver.name === name;
+    });
+
+    return matchingDrivers;
+}
+
+
+
+
 // Code your solution here
